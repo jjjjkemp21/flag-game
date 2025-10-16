edd1aeb10f2801b1ba6ec45adad9a7f4d8958b36
jjjjkemp21 <jjjjkemp21@gmail.com>
2025-10-16 00:03:03 -0700

    Fixed issue where container not on docker proxy network

--- deploy.sh
+++ deploy.sh
@@ -13,7 +13,7 @@ docker rm flag-game || true
 # Build the new Docker image
 docker build -t flag-game .
 
-# Run the new container, mapping internal port 80 to host port 8080
-docker run -d --restart always --name flag-game -p 8080:80 flag-game
+# Run the new container on the shared proxy network
+docker run -d --restart always --name flag-game --network=my_proxy_network flag-game
 
 echo "🚀 Deployment finished successfully!"
